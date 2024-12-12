d336442944aab39d79d76ec9fbeb8437ec818b8e
A-J8 <alvarojara986@gmail.com>
2024-12-12 17:23:42 -0300

    ajuste url mongo

--- Proyecto/backend/src/config/db.py
+++ Proyecto/backend/src/config/db.py
@@ -1,3 +1,3 @@
 from pymongo import MongoClient
 
-conn = MongoClient("host.docker.internal:27017") #modificar en base a la base de datos, ahora esta asi para que el docker acceda al mongo de mi maquina
+conn = MongoClient("mongodb://34.176.135.227:27017") #modificar en base a la base de datos, ahora esta asi para que el docker acceda al mongo de mi maquina
